61de895b85a016456157ec754b61351564e79271
artyom <Xangel2008@gmail.com>
2018-03-11 12:26:42 +0200

    - fix ci

--- setup.py
+++ setup.py
@@ -1,93 +1,37 @@
+#!/usr/bin/env python
+from setuptools import setup
 import sys
-import io
 
-from setuptools import setup, find_packages, __version__ as setuptools_version
-from pkg_resources import parse_version
+version = '2.0.15'
+classifiers = ["Development Status :: 5 - Production/Stable",
+               "Environment :: Plugins",
+               "Intended Audience :: Developers",
+               "Programming Language :: Python",
+               "Programming Language :: Python :: 2.7",
+               "Programming Language :: Python :: 3",
+               "Programming Language :: Python :: 3.4",
+               "Programming Language :: Python :: Implementation :: PyPy",
+               "License :: OSI Approved :: Apache Software License",
+               "Topic :: Software Development :: Testing"]
 
-import pkg_resources
+if sys.version_info >= (2, 7):
+    install_requires = ["requests>=2.7.9", "coverage"]
+else:
+    install_requires = ["requests>=2.7.9", "coverage", "argparse"]
 
-try:
-    import _markerlib.markers
-except ImportError:
-    _markerlib = None
-
-
-# _markerlib.default_environment() obtains its data from _VARS
-# and wraps it in another dict, but _markerlib_evaluate writes
-# to the dict while it is iterating the keys, causing an error
-# on Python 3 only.
-# Replace _markerlib.default_environment to return a custom dict
-# that has all the necessary markers, and ignores any writes.
-
-class Python3MarkerDict(dict):
-
-    def __setitem__(self, key, value):
-        pass
-
-    def pop(self, i=-1):
-        return self[i]
-
-
-if _markerlib and sys.version_info[0] == 3:
-    env = _markerlib.markers._VARS
-    for key in list(env):
-        new_key = key.replace('.', '_')
-        if new_key != key:
-            env[new_key] = env[key]
-
-    _markerlib.markers._VARS = Python3MarkerDict(env)
-
-    def default_environment():
-        return _markerlib.markers._VARS
-
-    _markerlib.default_environment = default_environment
-
-# Avoid the very buggy pkg_resources.parser, which doesnt consistently
-# recognise the markers needed by this setup.py
-# Change this to setuptools 20.10.0 to support all markers.
-if pkg_resources:
-    if parse_version(setuptools_version) < parse_version('20.10.0'):
-        MarkerEvaluation = pkg_resources.MarkerEvaluation
-
-        del pkg_resources.parser
-        pkg_resources.evaluate_marker = MarkerEvaluation._markerlib_evaluate
-        MarkerEvaluation.evaluate_marker = MarkerEvaluation._markerlib_evaluate
-
-
-setup(
-    name='tox-travis',
-    description='Seamless integration of Tox into Travis CI',
-    author='Ryan Hiebert',
-    author_email='ryan@ryanhiebert.com',
-    url='https://github.com/tox-dev/tox-travis',
-    license='MIT',
-    version='0.10',
-    package_dir={'': 'app'},
-    packages=find_packages('app'),
-    entry_points={
-        'tox': ['travis = tox_travis.hooks'],
-    },
-    install_requires=['tox>=2.0'],
-    extras_require={
-        ':python_version=="3.2"': ['virtualenv<14', 'pytest<3'],
-        ':platform_python_implementation=="PyPy" and python_version=="3.3"': ['virtualenv>=15.0.2'],
-    },
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development :: Testing',
-    ],
-)
+setup(name='codecov',
+      version=version,
+      description="Hosted coverage reports for Github, Bitbucket and Gitlab",
+      long_description=None,
+      classifiers=classifiers,
+      keywords='coverage codecov code python java scala php',
+      author='@codecov',
+      author_email='hello@codecov.io',
+      url='http://github.com/codecov/codecov-python',
+      license='http://www.apache.org/licenses/LICENSE-2.0',
+      packages=['app'],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=install_requires,
+      tests_require=["unittest2"],
+      entry_points={'console_scripts': ['codecov=codecov:main']})
